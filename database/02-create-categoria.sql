dc8a248c607a394b507e3337f225727b5b473e0f
Joselino Fermin <jose.lozada@sulwork.com.br>
2020-04-15 17:57:02 -0300

    Nuevo script para la creacion de la tabla CATEGORIA

---
+++ database/02-create-categoria.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `categoria` (
+  `codigo` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+\
+INSERT INTO `` (`codigo`,`nome`) VALUES (1,'Lazer');
+INSERT INTO `` (`codigo`,`nome`) VALUES (2,'Alimentação');
+INSERT INTO `` (`codigo`,`nome`) VALUES (3,'Supermecado');
+INSERT INTO `` (`codigo`,`nome`) VALUES (4,'Farmácia');
+INSERT INTO `` (`codigo`,`nome`) VALUES (5,'Outros');
