41f6c0f333b32f1aaa6a82b7e71d3bfdecb70ed1
Andreina Diaz <andreinadc@gmail.com>
2020-04-22 17:11:33 -0300

    Actualización hasta video 5.6

--- algaworks-api/src/main/java/com/algaworksapi/algaworksapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ algaworks-api/src/main/java/com/algaworksapi/algaworksapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.algaworksapi.algaworksapi.exceptionhandler;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -27,7 +32,7 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-        String mensagemDesenvolvedor = ex.getCause().toString();
+        String mensagemDesenvolvedor = ex.getCause() != null ? ex.getCause().toString() : ex.toString();
 
         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 
@@ -40,6 +45,27 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
         List<Erro> erros = criarListaDeErros(ex.getBindingResult());
         return handleExceptionInternal(ex, erros,headers,HttpStatus.BAD_REQUEST,request);
+    }
+
+    @ExceptionHandler({EmptyResultDataAccessException.class}) //Podria enviar todas las excepciones separadas por parametros
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
+        //Esste metodo es para enviar 404 cuando intente eliminar un registro que no esta, de esta forma ess un error del usuario y no del servidor
+        String mensagemUsuario = messageSource.getMessage("recurso.nao-encotrado", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex); //Tiene que ser de la clase de la dependencia que exporte en el pom
+
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return  handleExceptionInternal(ex, erros, new HttpHeaders(),HttpStatus.NOT_FOUND,request);
+    }
+
+
+    @ExceptionHandler({ DataIntegrityViolationException.class})
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request){
+        String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.toString();
+
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return  handleExceptionInternal(ex, erros, new HttpHeaders(),HttpStatus.BAD_REQUEST,request);
     }
 
 
